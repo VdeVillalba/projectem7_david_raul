0f28918e06b91f9e1aa120ae1d6d2868caa9395a
V <villalba.garcia.david@alumnat.copernic.cat>
2019-12-04 10:59:35 +0100

    Hola

---
+++ app/src/main/java/cat/villalba/projectem7_david_raul/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.villalba.projectem7_david_raul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
