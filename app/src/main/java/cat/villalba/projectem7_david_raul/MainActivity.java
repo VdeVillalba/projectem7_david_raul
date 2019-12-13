0b21440a8757831963ea46da4535bbb9da2f1c92
V <villalba.garcia.david@alumnat.copernic.cat>
2019-12-13 16:35:17 +0100

    añadidas activites

--- app/src/main/java/cat/villalba/projectem7_david_raul/MainActivity.java
+++ app/src/main/java/cat/villalba/projectem7_david_raul/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mRegistro = findViewById(R.id.txt_registro);
 
-        mRegistro.setOnClickListener();
 
     }
 
