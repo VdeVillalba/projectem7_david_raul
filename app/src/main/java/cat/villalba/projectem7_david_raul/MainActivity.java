fd01033a346e8b98f5c451ede1717a4c86178f95
V <villalba.garcia.david@alumnat.copernic.cat>
2019-12-13 16:10:20 +0100

    Pantalla

--- app/src/main/java/cat/villalba/projectem7_david_raul/MainActivity.java
+++ app/src/main/java/cat/villalba/projectem7_david_raul/MainActivity.java
@@ -3,14 +3,24 @@ package cat.villalba.projectem7_david_raul;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRegistro = findViewById(R.id.txt_registro);
 
+        mRegistro.setOnClickListener();
+
+    }
+
+    public void onClick(View v) {
 
     }
 }
