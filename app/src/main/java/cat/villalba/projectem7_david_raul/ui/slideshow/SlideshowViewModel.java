7e0a06d9ddfe781308265ce6ed7b783282cd3037
Raul Vaquero Carmona <vaquero.carmona.raul@alumnat.copernic.cat>
2019-12-16 12:50:31 +0100

    espero que funcione

---
+++ app/src/main/java/cat/villalba/projectem7_david_raul/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package cat.villalba.projectem7_david_raul.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
