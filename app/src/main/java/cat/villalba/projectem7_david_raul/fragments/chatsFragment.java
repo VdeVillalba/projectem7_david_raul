66a1927c4591b58935dad7d61c2edcd8184e17d8
V <villalba.garcia.david@alumnat.copernic.cat>
2020-02-27 08:23:04 +0100

    cambios menores

--- app/src/main/java/cat/villalba/projectem7_david_raul/fragments/chatsFragment.java
+++ app/src/main/java/cat/villalba/projectem7_david_raul/fragments/chatsFragment.java
@@ -19,7 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
